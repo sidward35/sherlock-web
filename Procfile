41d668b7499972a644df1cdb1df72f26bb95f88b
sidward35 <mathursiddharth2000@gmail.com>
2021-08-18 19:55:37 -0700

    django unchained

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:server
+web: gunicorn sherlockweb.wsgi --log-file -
---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.text_input(label, value='', max_chars=None, key=None, type='default', help=None, autocomplete=None, on_change=None, args=None, kwargs=None)
+
+st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None)
