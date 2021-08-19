64a1d109289f4259dad34c76a2a5a02b9aff8140
sidward35 <mathursiddharth2000@gmail.com>
2021-08-18 21:37:08 -0700

    Clean up Heroku deployment

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import subprocess
 st.title('Sherlock Username Search')
 username = st.text_input('Username', value='', help='Enter your username')
 if st.button('Submit'):
-    st.text('Searching for \''+username+'\'...')
+    st.text('Searching for \''+username+'\'... (this might take a minute)')
     s = subprocess.check_output(["python", "sherlock", username])
     st.text(s.decode("utf-8"))
