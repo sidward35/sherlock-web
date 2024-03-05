b59371500b4b7eeb74bcd2695e158ab05bcd090e
Siddharth Mathur <mathursiddharth2000@gmail.com>
2024-03-05 01:40:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,6 @@ import streamlit.components.v1 as components
 import subprocess
 
 st.set_page_config(page_title='SUS', page_icon = 'https://user-images.githubusercontent.com/27065646/53551960-ae4dff80-3b3a-11e9-9075-cef786c69364.png')
-components.html(
-    """
-    <script src="https://cdn.signalfx.com/o11y-gdi-rum/latest/splunk-otel-web.js"
-    crossorigin="anonymous"></script>
-        <script>
-            SplunkRum.init({
-            beaconUrl: 'https://rum-ingest.us1.signalfx.com/v1/rum',
-            rumAuth: '{AUTH}',
-            app: 'sherlock-web'
-        });
-    </script>
-    """
-)
 st.title('Sherlock Username Search')
 username = st.text_input('Username', value='', help='Enter your username')
 if st.button('Submit'):
